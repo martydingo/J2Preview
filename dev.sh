2295bcc18d0fb53254c4460b750fe64974336467
Martin George <marty@dingo.foo>
2024-09-12 11:47:50 +0100

    commit

--- dev.sh
+++ dev.sh
@@ -1,1 +1,2 @@
-bun x tailwindcss -i ./j2preview/static/src/css/global.css -o ./j2preview/static/dist/css/global.css --watch
+bun x tailwindcss -i ./j2preview/static/src/css/global.css -o ./j2preview/static/dist/css/global.css --watch &
+python3 -m j2preview
